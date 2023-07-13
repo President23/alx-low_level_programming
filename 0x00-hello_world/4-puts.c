399fb8793a1b87926a12bc8ca0f42f0e06084dee
President23 <ndatjeanclaude@gmail.com>
2023-07-13 14:32:42 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Hello puts
+ * Return: 0 ( when inserted the main)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
