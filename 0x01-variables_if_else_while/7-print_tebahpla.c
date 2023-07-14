4c217f5d3c3680f82b0f609c64dcf1fb1cb5d294
President23 <ndatjeanclaude@gmail.com>
2023-07-14 15:16:36 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - start
+ * Return: 0 (done)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
