b527fcd8b21ce6163fadaaf9511999ee8ebd4f6a
President23 <ndatjeanclaude@gmail.com>
2023-07-14 16:19:02 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,17 +3,18 @@
  * main - start
  * return: 0 (when done)
  */
+
 int main(void)
 {
-	char n;
+	         char n;
 
-	n = 'a';
-	while
-		(n <= 'z') {
-			if ((n != 'q' && n != 'e') && n <= 'z')
-				putchar(n);
-			n++;
+		 n = 'a';
+		 while
+			 (n <= 'z') {
+				 if ((n != 'q' && n != 'e') && n <= 'z')
+					 putchar(n);
+				 n++;
 			}
-	putchar('\n');
-	return (0);
+		 putchar('\n');
+		 return (0);
 }
