a163c2cac917fd73759629799a400c2eff14e7e2
President23 <ndatjeanclaude@gmail.com>
2023-08-06 10:10:51 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - get a name
+ * @argc: that counts the number of insert in terminal
+ * @argv: array counts insertation in argc
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
