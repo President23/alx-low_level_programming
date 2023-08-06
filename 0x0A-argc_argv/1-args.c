3a3b3865179f533ef65ac9476d63c9ddcf638eaa
President23 <ndatjeanclaude@gmail.com>
2023-08-06 10:29:37 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print
+ * @argc: the number of things inserted in terminal
+ * @argv: the array that number insertation in arg stuff
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
