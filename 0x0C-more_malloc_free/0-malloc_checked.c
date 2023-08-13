df36a4c1ed52db8d2624fffdf8451cf63a893e6c
President23 <ndatjeanclaude@gmail.com>
2023-08-13 12:57:24 -0700

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - mem
+ * @b: size
+ * Return: .
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *g;
+
+	g = malloc(b);
+
+	if (g == NULL)
+		exit(98);
+
+	return (g);
+}
