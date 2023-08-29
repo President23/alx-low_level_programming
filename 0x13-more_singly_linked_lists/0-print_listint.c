e6ae2681cd9341c73dff5950c8af70bee2c33203
President23 <ndatjeanclaude@gmail.com>
2023-08-29 11:34:30 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - prints
+ * @h: a ptr
+ * Return: nbr of nd
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t sth = 0;
+	
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		sth++;
+		h = h->next;
+	}
+	
+	return (sth);
+}
